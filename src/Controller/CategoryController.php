c6d7cec574ea2f2edbcd1dbd310a1e67197ef16d
juan-gil-05 <gilj06@hotmail.com>
2025-06-14 17:51:35 +0200

    Modify of Food and Category CRUD in order to use the serialize pack

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,22 +7,30 @@ use App\Repository\CategoryRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
-#[ROUTE("api/category", name:'api_category_')]
+#[ROUTE("api/category", name: 'api_category_')]
 final class CategoryController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $manager, private CategoryRepository $repository) {}
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private CategoryRepository $repository,
+        private SerializerInterface $serializer,
+        private UrlGeneratorInterface $urlGenerator
+    ) {}
 
     // Create
     #[ROUTE("/", name: 'new', methods: 'POST')]
-    public function new(): Response
+    public function new(Request $request): JsonResponse
     {
-
-        $category = new Category;
-
-        $category->setTitle("Dessert");
+        // To get the content of the request and deserialize it into a category object 
+        $category = $this->serializer->deserialize($request->getContent(), Category::class, 'json');
         $category->setCreatedAt(new DateTimeImmutable());
 
         // To save data into DB
@@ -31,47 +39,60 @@ final class CategoryController extends AbstractController
         // To send data to DB
         $this->manager->flush();
 
-        return $this->json(
-            [
-                "message" => "category created with {$category->getId()} id"
-            ],
-            Response::HTTP_CREATED,
+        // To serialize the object into Json, and send it as the response
+        $responseData = $this->serializer->serialize($category, 'json');
+        // To redirect the client into the page with the new category
+        $location = $this->urlGenerator->generate(
+            "api_category_show",
+            ['id' => $category->getId()],
+            UrlGeneratorInterface::ABSOLUTE_URL
         );
+
+        return new JsonResponse($responseData, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 
     // Read
     #[ROUTE("/{id}", name: 'show', methods: 'GET')]
-    public function show($id): Response
+    public function show($id): JsonResponse
     {
-
+        // To search the object by ID
         $category = $this->repository->findOneBy(["id" => $id]);
 
-        if (!$category) {
-            throw $this->createNotFoundException("No category found for {$id} id");
-        }
+        if ($category) {
+            // To serialize the category object, in order to send it as a JsonResponse 
+            $responseData = $this->serializer->serialize($category, 'json');
 
-        return $this->json(
-            ['message' => "A category was found with the name: {$category->getTitle()} with {$category->getId()} id"]
-        );
+            return new JsonResponse($responseData, Response::HTTP_OK, [], true);
+        }
+        // If it wasn't found
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 
     // Update
     #[ROUTE("/{id}", name: 'edit', methods: 'PUT')]
-    public function edit($id): Response
+    public function edit($id, Request $request): Response
     {
-
+        // To search the object by ID
         $category = $this->repository->findOneBy(["id" => $id]);
 
-        if (!$category) {
-            throw $this->createNotFoundException("No category found for {$id} id");
+        if ($category) {
+            // To get the content sent and deserialize it into a category object
+            $this->serializer->deserialize(
+                $request->getContent(),
+                Category::class,
+                'json',
+                // To update the existing object instead of creating a new one
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $category]
+            );
+            $category->setUpdatedAt(new DateTimeImmutable());
+
+            // To update the DB
+            $this->manager->flush();
+
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
         }
 
-        $category->setTitle("UPDATED 2");
-        $this->manager->flush();
-
-        return $this->json(
-            ['message' => "A category was updated: {$category->getId()} id"]
-        );
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 
     // Delete
@@ -89,7 +110,7 @@ final class CategoryController extends AbstractController
         $this->manager->flush();
 
         return $this->json(
-            ['message' => "A category was deleted with id"], 
+            ['message' => "A category was deleted with id"],
             Response::HTTP_NO_CONTENT
         );
     }
--- src/Controller/FoodController.php
+++ src/Controller/FoodController.php
@@ -7,24 +7,30 @@ use App\Repository\FoodRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[ROUTE("api/food", name:'api_food_')]
 final class FoodController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $manager, private FoodRepository $repository) {}
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private FoodRepository $repository,
+        private SerializerInterface $serializer,
+        private UrlGeneratorInterface $urlGenerator
+    ) {}
 
     // Create
     #[ROUTE("/", name: 'new', methods: 'POST')]
-    public function new(): Response
+    public function new(Request $request): JsonResponse
     {
-
-        $food = new Food;
-
-        $food->setTitle("Raclette");
-        $food->setDescription("A french food");
-        $food->setPrice(10);
+        // To get the content of the request and deserialize it into a food object 
+        $food = $this->serializer->deserialize($request->getContent(), Food::class, 'json');
         $food->setCreatedAt(new DateTimeImmutable());
 
         // To save data into DB
@@ -33,47 +39,60 @@ final class FoodController extends AbstractController
         // To send data to DB
         $this->manager->flush();
 
-        return $this->json(
-            [
-                "message" => "new food created with {$food->getId()} id"
-            ],
-            Response::HTTP_CREATED,
+        // To serialize the object into Json, and send it as the response
+        $responseData = $this->serializer->serialize($food, 'json');
+        // To redirect the client into the page with the new food
+        $location = $this->urlGenerator->generate(
+            "api_food_show",
+            ['id' => $food->getId()],
+            UrlGeneratorInterface::ABSOLUTE_URL
         );
+
+        return new JsonResponse($responseData, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 
     // Read
     #[ROUTE("/{id}", name: 'show', methods: 'GET')]
-    public function show($id): Response
+    public function show($id): JsonResponse
     {
-
+        // To search the object by ID
         $food = $this->repository->findOneBy(["id" => $id]);
 
-        if (!$food) {
-            throw $this->createNotFoundException("No food found for {$id} id");
-        }
+        if ($food) {
+            // To serialize the food object, in order to send it as a JsonResponse 
+            $responseData = $this->serializer->serialize($food, 'json');
 
-        return $this->json(
-            ['message' => "A food was found with the name: {$food->getTitle()} with {$food->getId()} id"]
-        );
+            return new JsonResponse($responseData, Response::HTTP_OK, [], true);
+        }
+        // If it wasn't found
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 
     // Update
     #[ROUTE("/{id}", name: 'edit', methods: 'PUT')]
-    public function edit($id): Response
+    public function edit($id, Request $request): Response
     {
-
+        // To search the object by ID
         $food = $this->repository->findOneBy(["id" => $id]);
 
-        if (!$food) {
-            throw $this->createNotFoundException("No food found for {$id} id");
+        if ($food) {
+            // To get the content sent and deserialize it into a food object
+            $this->serializer->deserialize(
+                $request->getContent(),
+                Food::class,
+                'json',
+                // To update the existing object instead of creating a new one
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $food]
+            );
+            $food->setUpdatedAt(new DateTimeImmutable());
+
+            // To update the DB
+            $this->manager->flush();
+
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
         }
 
-        $food->setDescription("UPDATED 2");
-        $this->manager->flush();
-
-        return $this->json(
-            ['message' => "A food was updated: {$food->getDescription()} with {$food->getId()} id"]
-        );
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 
     // Delete
@@ -91,7 +110,7 @@ final class FoodController extends AbstractController
         $this->manager->flush();
 
         return $this->json(
-            ['message' => "A food was deleted with id"], 
+            ['message' => "A food was deleted with id"],
             Response::HTTP_NO_CONTENT
         );
     }
