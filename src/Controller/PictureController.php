4b5024553425696e88fd556e29fab159b2799610
juan-gil-05 <gilj06@hotmail.com>
2025-06-22 18:58:02 +0200

    Add of edit, and delete functions to picture controller

--- src/Controller/PictureController.php
+++ src/Controller/PictureController.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use OpenApi\Attributes as OA;
-
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 #[ROUTE("api/picture", name: "api_picture_")]
 class PictureController extends AbstractController
@@ -87,7 +87,7 @@ class PictureController extends AbstractController
         return new JsonResponse($responseData, Response::HTTP_CREATED, [], true);
     }
 
-    // Show
+    // Read
     #[ROUTE("/{id}", name: "show", methods: "GET")]
     #[OA\Get(
         path: '/api/picture/{id}',
@@ -124,5 +124,99 @@ class PictureController extends AbstractController
         $responseData = $this->serializer->serialize($picture, 'json', ["groups" => ["Picture:read"]]);
 
         return new JsonResponse($responseData, Response::HTTP_OK, [], true);
+    }
+
+    // Update
+    #[ROUTE("/{id}", name: 'edit', methods: 'PUT')]
+    #[OA\Put(
+        path: '/api/picture/{id}',
+        summary: "Edit a picture",
+        requestBody: new OA\RequestBody(
+            required: true,
+            description: "picture data to edit",
+            content: new OA\JsonContent(
+                properties: [
+                    new OA\Property(property: "name", type: "string", example: "NEW picture name"),
+                    new OA\Property(property: "slug", type: "string", example: "NEW_picture_slug"),
+                ],
+                type: "object"
+            )
+        ),
+        responses: [
+            new OA\Response(
+                response: 204,
+                description: "picture edited successfully",
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(property: "name", type: "string", example: "picture name edited"),
+                        new OA\Property(property: "slug", type: "string", example: "picture slug edited"),
+                    ],
+                    type: "object"
+                )
+            ),
+            new OA\Response(
+                response: 404,
+                description: "picture not found",
+            )
+        ]
+    )]
+    public function edit($id, Request $request): Response
+    {
+        // To search the object by ID
+        $picture = $this->pictureRepo->findOneBy(["id" => $id]);
+
+        if ($picture) {
+            // To get the content sent and deserialize it into a picture object
+            $this->serializer->deserialize(
+                $request->getContent(),
+                Picture::class,
+                'json',
+                // To update the existing object instead of creating a new one
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $picture]
+            );
+            $picture->setUpdatedAt(new DateTimeImmutable());
+
+            // To update the DB
+            $this->manager->flush();
+
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        }
+
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
+
+    // Delete
+    #[ROUTE("/{id}", name: 'delete', methods: 'DELETE')]
+    #[OA\Delete(
+        path: '/api/picture/{id}',
+        summary: "Delete a picture",
+        responses: [
+            new OA\Response(
+                response: 204,
+                description: "picture deleted successfully",
+            ),
+            new OA\Response(
+                response: 404,
+                description: "picture not found",
+            )
+        ]
+
+    )]
+    public function delete($id): Response
+    {
+
+        $picture = $this->pictureRepo->findOneBy(["id" => $id]);
+
+        if (!$picture) {
+            throw $this->createNotFoundException("No picture found for {$id} id");
+        }
+
+        $this->manager->remove($picture);
+        $this->manager->flush();
+
+        return new JsonResponse(
+            ['message' => "A picture was deleted with id"],
+            Response::HTTP_NO_CONTENT
+        );
     }
 }
